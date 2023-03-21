c68eba48efc0020c0f76590153a34d6dcd8878cf
Luis Vasquez <luisvbdr0301@gmail.com>
2023-03-21 10:51:01 -0400

    feat: implement hotjar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 
 import { Poppins, Raleway } from "@next/font/google";
+import { hotjar } from "react-hotjar";
 import Script from "next/script";
+import { useEffect } from "react";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 const raleway = Raleway({ weight: "500", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(3416958, 6);
+  }, []);
+
   return (
     <div className={`${raleway.className} ${poppins.className}`}>
       <Script
